cf4a314208ff826384ee329ba5acfe67da8a254f
Kristyan Yochev <kris.yochev@gmail.com>
2020-06-09 21:29:29 +0300

    Added creation of subreddits

--- backend/reddit/resources/subreddits.py
+++ backend/reddit/resources/subreddits.py
@@ -1,8 +1,19 @@
-from flask_restful import Resource, request
+from flask_restful import Resource, request, reqparse
 from reddit.models.subreddit import Subreddit
+from sqlite3 import IntegrityError
 
 
 class Subreddits(Resource):
+    def __init__(self):
+        super().__init__()
+        self.parser = reqparse.RequestParser()
+        self.parser.add_argument(
+            "name",
+            type=str,
+            required=True,
+            help="Name is required!"
+        )
+
     def get(self):
         args: dict = request.args
 
@@ -11,3 +22,17 @@ class Subreddits(Resource):
         subreddits = Subreddit.search(search_term)
 
         return list(map(Subreddit.toJSON, subreddits))
+
+    def post(self):
+        name = self.parser.parse_args().get("name")
+
+        try:
+            Subreddit(name).addToDb()
+        except IntegrityError:
+            return {
+                "message": {
+                    "error": "Subreddit already exists"
+                }
+            }, 400
+
+        return {}, 200

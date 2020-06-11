54044848d7114990be70400c0ecd53b13b4084db
Kristyan Yochev <kris.yochev@gmail.com>
2020-06-11 22:18:50 +0300

    Added login page and functionality

--- reddit/extensions.py
+++ reddit/extensions.py
@@ -1,7 +1,3 @@
-from flask_restful import Api
-from flask_jwt_extended import JWTManager
 from reddit.database.database import Database
 
-api = Api()
-jwt = JWTManager()
 db = Database("database.db")
--- run.py
+++ run.py
@@ -1,8 +1,6 @@
-from reddit.app import create_app
+from reddit.app import app
 from reddit.database.init import create_tables
 
 if __name__ == "__main__":
-    app = create_app()
     create_tables()
-
     app.run(port=3000)

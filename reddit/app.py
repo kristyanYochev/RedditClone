7a7b69ee8a82ddb573caa5b0c843d40db2dba112
Kristyan Yochev <kris.yochev@gmail.com>
2020-06-11 22:32:26 +0300

    Added registration

--- reddit/app.py
+++ reddit/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, render_template, redirect, session
 
 from reddit.models.user import User, UserNotFoundError, InvalidPasswordError
+from sqlite3 import IntegrityError
 
 
 def create_app() -> Flask:
@@ -14,7 +15,10 @@ app = create_app()
 
 @app.route("/")
 def index():
-    return "<h1>Hello World</h1><a href='/login'>Login</a>"
+    return """
+        <h1>Hello World</h1>
+        <a href='/login'>Login</a>
+        <a href='/register'>Register</a>"""
 
 
 @app.route("/login", methods=["GET", "POST"])
@@ -36,3 +40,26 @@ def login():
             return render_template("login.html", error="User not found!")
         except InvalidPasswordError:
             return render_template("login.html", error="Invalid password!")
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "GET":
+        return render_template("register.html", error=None)
+    else:
+        username: str = request.form["username"]
+        password: str = request.form["password"]
+        repeat_password: str = request.form["repeat_password"]
+
+        if password != repeat_password:
+            return render_template(
+                "register.html",
+                error="Passwords do not match!"
+            )
+
+        try:
+            User.register(username, password)
+
+            return redirect("/login")
+        except IntegrityError:
+            return render_template("register.html", error="Username taken!")
